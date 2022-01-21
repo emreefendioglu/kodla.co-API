01f52d3de96cae66e5acf2c1f22c174e593d84e7
Emre Efendioğlu <emre@unicrow.com>
2022-01-21 18:32:10 +0300

    express init

--- express/server.js
+++ express/server.js
@@ -1,11 +1,42 @@
 const Express = require("express");
 
+const users = require("./users");
+
+/**
+ * Express App
+ */
 const app = Express();
 
-app.all("/", function(req, res) {
+app.use("/pictures", Express.static(__dirname+"/pictures"));
+
+app.get("/users/", function(req, res) {
+    var data = users;
+    if(req.query.gender) {
+        data = data.filter(user => user.gender === req.query.gender)
+    }
+
+    const result = {
+        count: data.length,
+        filter: req.query,
+        users: data
+    }
+
     res
         .status(200)
-        .send({});
+        .send(result)
+});
+
+app.get("/users/:id([0-9]+)", function(req, res) {
+    const result = users[req.params.id];
+
+    if(!result)
+        res
+            .status(404)
+            .send({message: "not-found"})
+
+    res
+        .status(200)
+        .send(result)
 });
 
 app.listen(8000, function() {
--- server/json-handler.js
+++ server/json-handler.js
@@ -3,6 +3,5 @@ module.exports = function(req, res) {
     res.setHeader("Content-Type", "text/json");
     const data = {"name": "emre"};
 
-    res.write(JSON.stringify(data));
-    res.end();
+    res.end(JSON.stringify(data));
 }
